61dbfd5146c3d78e2fb4f776b3d5d676a3e3f594
Ivo Katunarić <ivo@shoutem.com>
2017-01-17 11:36:45 +0100

    Avoid postinstall script failure

--- src/scripts/postinstall.js
+++ src/scripts/postinstall.js
@@ -6,9 +6,15 @@ import rimraf from 'rmfr';
 (async () => {
   const cliHome = path.join(os.homedir(), '.shoutem');
 
-  await rimraf(path.join(cliHome, 'mobile-env'));
-  await rimraf(path.join(cliHome, 'mobile-app'));
-  await rimraf(path.join(cliHome, 'api-token'));
-  await rimraf(path.join(cliHome, 'dev-info'));
-  await rimraf(path.join(cliHome, 'last-error.json'));
+  try {
+    await Promise.all([
+      rimraf(path.join(cliHome, 'mobile-env')),
+      rimraf(path.join(cliHome, 'mobile-app')),
+      rimraf(path.join(cliHome, 'api-token')),
+      rimraf(path.join(cliHome, 'dev-info')),
+      rimraf(path.join(cliHome, 'last-error.json'))
+    ]);
+  } catch (err) {
+    console.error(err);
+  }
 })();
