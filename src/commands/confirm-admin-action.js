177b2c6c8dce06d513da67145a0751c24e51b2dc
Ivo Katunarić <ivo@shoutem.com>
2017-03-03 14:33:17 +0100

    Skip root push confirmation on non-production

--- src/commands/confirm-admin-action.js
+++ src/commands/confirm-admin-action.js
@@ -1,10 +1,11 @@
 import { ensureDeveloperIsRegistered } from '../commands/register';
 import confirm from '../extension/confirmer';
+import { getHostEnvName } from '../clients/server-env';
 
 export default async function(msg) {
   const dev = await ensureDeveloperIsRegistered();
 
-  if (dev.name !== 'shoutem') {
+  if (getHostEnvName() !== 'production' || dev.name !== 'shoutem') {
     return true;
   }
 
