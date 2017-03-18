52876519412b17c34a1158049d85fc17c2a59f96
Ivo Katunarić <ivo@shoutem.com>
2017-03-18 16:22:20 +0100

    Change defaults for local CLI mode

--- config/services.js
+++ config/services.js
@@ -14,12 +14,12 @@ const envValues = {
     host: 'qa.sauros.hr'
   },
   local: {
-    host: process.env.SHOUTEM_CLI_HOST || 'dev.sauros.hr',
-    appBuilder: process.env.SHOUTEM_CLI_APP_BUILDER,
-    appManager: process.env.SHOUTEM_CLI_APP_MANAGER,
-    authService: process.env.SHOUTEM_CLI_AUTH_SERVICE,
-    extensionManager: process.env.SHOUTEM_CLI_EXTENSION_MANAGER,
-    legacyService: process.env.SHOUTEM_CLI_LEGACY_SERVICE,
+    host: process.env.SHOUTEM_CLI_HOST || 'shoutem.local',
+    appBuilder: process.env.SHOUTEM_CLI_APP_BUILDER || 'http://builder.shoutem.local:3000',
+    appManager: process.env.SHOUTEM_CLI_APP_MANAGER || 'http://localhost:3003',
+    authService: process.env.SHOUTEM_CLI_AUTH_SERVICE || 'http://api.shoutem.local',
+    extensionManager: process.env.SHOUTEM_CLI_EXTENSION_MANAGER || 'http://localhost:3002',
+    legacyService: process.env.SHOUTEM_CLI_LEGACY_SERVICE || 'http://api.shoutem.local',
   }
 };
 
