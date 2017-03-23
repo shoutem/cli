f189c34d448998a9d28ca453a2031986bbe426ce
Ivo Katunaric <ivo@shoutem.com>
2017-03-23 11:43:10 +0100

    Fix transpilation for node4

--- src/shoutem.js
+++ src/shoutem.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
+
+const semver = require('semver');
+
+const presets = [];
+const plugins = ['transform-object-rest-spread', 'transform-async-to-generator'];
+
+if (semver.lt(process.versions.node, '6.0.0')) {
+  presets.push('full-node4');
+} else if (semver.lt(process.versions.node, '7.0.0')) {
+  presets.push('node6');
+} else {
+  presets.push('node7');
+}
+
 require('babel-register')({
-  presets: ['node6'],
-  plugins: ['transform-object-rest-spread', 'transform-async-to-generator'],
-  sourceMaps: true,
+  presets,
+  plugins,
   ignore: false,
   only: ['config', 'src', 'templates']
 });
