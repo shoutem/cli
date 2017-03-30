37454c5c1c7e8576e2cad2e79949897de72c4050
Tomislav Tenodi <tomislav.tenodi@gmail.com>
2017-03-30 14:59:28 +0200

    Position Hello World to center

--- templates/screen/app/screens/{{screenName}}.js
+++ templates/screen/app/screens/{{screenName}}.js
@@ -3,13 +3,28 @@ import React, {
 } from 'react';
 
 import {
+  StyleSheet,
   Text,
+  View
 } from 'react-native';
 
 export default class {{screenClassName}} extends Component {
   render() {
     return (
-      <Text>Hello World!</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World!</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
