f3c6d1bafa39fb8c796cba0b6f4b9244c2c4cd50
Ivo Katunaric <ivo@shoutem.com>
2017-04-04 09:38:10 +0200

    Fail on node 6 or older

--- src/shoutem.js
+++ src/shoutem.js
@@ -4,6 +4,11 @@ const semver = require('semver');
 const path = require('path');
 const os = require('os');
 
+if (semver.lt(process.versions.node, '7.0.0')) {
+  console.error(`You appear to be using Node v${process.versions.node}, however, Node 7 or later is required`);
+  return;
+}
+
 const babelCachePath = path.join(os.homedir(), '.shoutem', 'cache', 'babel-cache');
 process.env.BABEL_CACHE_PATH = process.env.BABEL_CACHE_PATH || babelCachePath;
 
