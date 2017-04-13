5b5e49b09831a67eaffa2b0cfe41e882c7c10f59
Ivo Katunaric <ivo@shoutem.com>
2017-04-13 17:28:03 +0200

    Add build-ios command

--- src/cli/build-ios.js
+++ src/cli/build-ios.js
@@ -1,8 +1,8 @@
 import buildApp from '../commands/build';
 import { handleError } from '../extension/error-handler';
 
-export const description = 'Build android app for production';
-export const command = 'build-android [appId]';
+export const description = 'Build iOS app for production';
+export const command = 'build-ios [appId]';
 export const builder = yargs => {
   return yargs.options({
     noclean: {
@@ -17,8 +17,14 @@ export const builder = yargs => {
 };
 
 export async function handler(args) {
+  if (process.platform !== 'darwin') {
+    console.log('Unfortunately, Apple only lets you build an iOS app on a Mac. ' +
+      'However, Shoutem can build it for you. Go inside Shoutem Builder and click "Publish".');
+    return null;
+  }
+
   try {
-    await buildApp('android', args);
+    await buildApp('ios', args);
   } catch (err) {
     await handleError(err);
   }
