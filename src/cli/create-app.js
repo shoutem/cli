983b8029d6262641653035b172a97b60560124b2
Ivo Katunarić <ivo@shoutem.com>
2017-05-22 18:46:55 +0200

    Add placeholder for the 'create app' command

---
+++ src/cli/create-app.js
@@ -0,0 +1,6 @@
+export const description = 'Bootstrap React Native app';
+export const command = 'create app';
+export function handler() {
+  console.log("We're just polishing the app creating process.\nFor now, use Shoutem Builder to create app: " +
+    'https://builder.shoutem.com'.bold.white);
+}
