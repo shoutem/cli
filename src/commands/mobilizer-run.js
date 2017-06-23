13c3ed350df57c0b8bba2b58c497ddea0c5e5aa7
Ivo Katunarić <ivo@shoutem.com>
2017-06-23 16:31:20 +0200

    Add watchman hint to failed shoutem run commands

--- src/commands/mobilizer-run.js
+++ src/commands/mobilizer-run.js
@@ -2,12 +2,16 @@ import { exec } from 'mz/child_process';
 import { handleError } from '../extension/error-handler';
 import * as platformManager from '../extension/platform-manager';
 import { ensureUserIsLoggedIn } from './login';
+import commandExists from '../extension/command-exists';
 
 export default async function (options) {
   try {
     await ensureUserIsLoggedIn();
     await platformManager.mobilizerRun({ ...options, path: process.cwd() });
   } catch (err) {
+    if (!/^win/.test(process.platform) && !await commandExists('watchman')) {
+      console.log('HINT: You should probably install Facebook\'s `watchman` before running react-native commands'.bold.yellow);
+    }
     await handleError(err);
   }
 }
