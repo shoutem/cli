ba1348e8329a8dbe1952009c140dc570a18af5b2
Ivo Katunaric <ivo@shoutem.com>
2017-06-23 16:45:57 +0200

    Change create-app message and change rebuild to pull

--- src/cli/create-app.js
+++ src/cli/create-app.js
@@ -1,6 +1,6 @@
 export const description = 'Bootstrap React Native app';
 export const command = 'create app';
 export function handler() {
-  console.log("We're just polishing the app creating process.\nFor now, use Shoutem Builder to create app: " +
-    'https://builder.shoutem.com'.bold.white);
+  console.log('We have a new app creation process:' +
+    'http://shoutem.github.io/docs/extensions/tutorials/getting-started'.bold.white);
 }
--- src/cli/pull.js
+++ src/cli/pull.js
@@ -2,8 +2,8 @@ import { executeAndHandleError } from '../extension/error-handler';
 import { getPlatformRootDir } from '../extension/platform';
 import * as npm from '../extension/npm';
 
-export const description = 'Runs platform\'s configure script to sync with changes to native code';
-export const command = 'rebuild';
+export const description = 'Runs platform\'s configure script to sync with changes to native code and server';
+export const command = 'pull';
 export const builder = yargs => {
   return yargs
     .usage(`shoutem ${command} \n\n${description}`);
