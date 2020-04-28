49d80bc5629def2795177392773d58239ecca6e5
Aleksandra Mladenovic <aleksandra.mladenovic@five.agency>
2020-04-29 01:17:47 +0200

    Revert to commit 00442bbeee253ca739831ba4fa60998013c8f070

--- src/shoutem.js
+++ src/shoutem.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
-
 const semver = require('semver');
 const path = require('path');
-const getHomeDir = require('./home-dir');
-const packageJson = require('../package.json');
 require('colors');
 
+const getHomeDir = require('./home-dir');
+const packageJson = require('../package.json');
+
 const homeDir = getHomeDir();
+const nodeVer = process.versions.node;
+const cliArgs = process.argv.slice(2);
+
+if (cliArgs[0] === '-v' || cliArgs[0] === '--version') {
+  console.log(packageJson.version);
+  process.exit(0);
+}
 
 if (!path.isAbsolute(homeDir)) {
   console.log(`ERROR: path ${homeDir} is not an absolute path.`.red);
@@ -14,13 +21,17 @@ if (!path.isAbsolute(homeDir)) {
   process.exit(1);
 }
 
-if (semver.lt(process.versions.node, '6.0.0')) {
-  console.error(`You appear to be using Node v${process.versions.node}, however, Node 6 or later is required`);
-  return;
+if (semver.lt(nodeVer, '6.0.0')) {
+  console.error(`You appear to be using Node v${nodeVer}, however, Node 6 or later is required`);
+  process.exit(1);
 }
 
-const babelCachePath = path.join(homeDir, 'cache', 'babel-cache');
-process.env.BABEL_CACHE_PATH = process.env.BABEL_CACHE_PATH || babelCachePath;
+process.env.SHOUTEM_CLI_DIRNAME = path.basename(__dirname);
 
-require('babel-register')(packageJson.babel);
+if (process.env.SHOUTEM_CLI_DIRNAME === 'src') {
+  const babelCachePath = path.join(homeDir, 'cache', 'babel-cache');
+  process.env.BABEL_CACHE_PATH = process.env.BABEL_CACHE_PATH || babelCachePath;
+  require('babel-register')(packageJson.babel);
+}
+
 require('./cli');
