996637ce0ade414853c63b58435ac54fdea532d3
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2020-07-27 10:43:04 +0200

    Fix const naming conflict

--- src/services/node.js
+++ src/services/node.js
@@ -1,15 +1,14 @@
 import _ from 'lodash';
+import {
+  packageManager as defaultPackageManager
+} from './package-manager-service';
 import * as packageManager from './package-manager-service';
 import { exec } from 'child-process-promise'
 
-// TODO: Switch to using this from command or from cached CLI settings
-// const packageManager = getPackageManager();
-const packageManager = 'yarn';
-
 export async function containsBuildTask(dir) {
   try {
-    const pkgJson = await packageManager.getPackageJson(dir);
-    return !!_.get(pkgJson, 'scripts.build');
+    const packageJson = await packageManager.getPackageJson(dir);
+    return !!_.get(packageJson, 'scripts.build');
   } catch (err) {
     return false;
   }
@@ -21,8 +20,8 @@ export async function buildNodeProject(dir) {
   }
 
   try {
-    await exec(`${packageManager} install`, { cwd: dir, FORCE_COLOR: true });
-    await exec(`${packageManager} run build`, { cwd: dir, FORCE_COLOR: true });
+    await exec(`${defaultPackageManager} install`, { cwd: dir, FORCE_COLOR: true });
+    await exec(`${defaultPackageManager} run build`, { cwd: dir, FORCE_COLOR: true });
   } catch (err) {
     console.log(err.stdout);
     console.error(err.stderr);
