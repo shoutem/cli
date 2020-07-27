6438419761e185c7be770923bde00f4825a36cfd
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2020-07-27 11:44:39 +0200

    Remove unneccessary console.log

--- src/clients/default-package-manager.js
+++ src/clients/default-package-manager.js
@@ -11,8 +11,6 @@ export function getDefaultPackageManager() {
   try {
     return fs.readFileSync(defaultPackageManagerFilePath, 'utf8');
   } catch (err) {
-    // an error is only likely to appear if the file doesn't exist
-    console.log("Error reading default package manager. Setting it to 'npm'.");
     setDefaultPackageManager('npm');
     return 'npm';
   }
