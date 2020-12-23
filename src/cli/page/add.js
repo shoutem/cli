47f9bbd448c346f73979796c1d8f2bae2a60db7a
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2020-12-23 12:52:55 +0100

    Adjust messaging for page add

--- src/cli/page/add.js
+++ src/cli/page/add.js
@@ -19,5 +19,5 @@ export async function createPage(opts, extensionPath) {
   const changes = await instantiateExtensionTemplate('settings-page', { ...opts, extensionPath, developer });
   await offerChanges(changes);
   console.log('Success'.green.bold);
-  console.log('Remember to create \'server/translations/en.json\' add your translation strings to it.\nYou can use example.json to check the format.');
+  console.log('Remember to create \'server/translations/en.json\' add your translation strings to it.\nYou can use \'server/translations/example.json\' to check the format.');
 }
