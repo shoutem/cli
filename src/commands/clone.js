0133e6f30c7153b04acd2b755e2541f64a2442c1
Definitely-Not-Vlad <vlad@definitely-not-vlad.com>
2021-03-08 12:34:48 +0100

    Fix number of loops

--- src/commands/clone.js
+++ src/commands/clone.js
@@ -43,7 +43,7 @@ export async function pullExtensions(appId, destinationDir) {
 
 async function pullExtension(destinationDir, { extension, canonicalName }) {
   let pullError = {};
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 4; i++) {
     try {
       const url = await getExtensionUrl(extension);
       const tgzDir = (await tmp.dir()).path;
