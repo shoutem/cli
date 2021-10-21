d9f9b222a038bd859fa49b561541ca111a47b23b
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2021-10-21 11:11:47 +0200

    Deprecate push command (#121)
    * Add deprecation warning for push command
    
    * Clarity edit for push deprecation warning

--- src/cli/push.js
+++ src/cli/push.js
@@ -43,6 +43,7 @@ export const handler = args => executeAndHandleError(async () => {
     return;
   }
 
+  console.warn('DEPRECATED: \'shoutem push\' is deprecated and will be removed with the next major version of the Shoutem CLI. Shoutem now supports release candidate versioning, which allows developers to \'shoutem publish\', for example, 1.0.0-rc.0 versioned extensions which are labeled as "Beta" versions in the Shoutem Builder. App owners who update to RC versions also have the option to revert to the last "stable" (non-rc) version.');
   args.paths = multiglob(args.paths);
   await pushAll(args);
 });
