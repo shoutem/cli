6e8717c31157f3740cc94b2c3b8843087411844f
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2021-11-24 15:20:28 +0100

    Feature/reopens (#128)
    * Move eslint from devDeps to deps
    
    * Fix extension linting and push deprecation format/color.

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -1,11 +1,11 @@
 import * as extensionManager from '../clients/extension-manager';
 import * as utils from '../services/extension';
-import { uploadExtension } from '../commands/push';
 import { getExtensionCanonicalName } from '../clients/local-extensions';
 import msg from '../user_messages';
 import { spinify } from '../services/spinner';
 import { getPlatformRootDir } from '../services/platform';
 import { offerInstallationUpdate } from '../cli/extension/publish';
+import { uploadExtension } from './push';
 
 export async function publishExtension(extDir) {
   const extJson = await utils.loadExtensionJson(extDir);
